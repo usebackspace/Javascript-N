124d31d9b0eca3598a91265e876011917d194e63
Shivam <schaurasia@infoane.com>
2024-09-12 18:28:58 +0530

    update

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-// === push() method ===
+// =================== push() method ===================
 console.log('========= push() =============')
 
 let fruits = ['apple', 'banana'];
@@ -6,9 +6,9 @@ console.log(fruits)
 fruits.push('orange', 'grape');
 console.log(fruits)
 
-console.log('========= pop() ============')
 
-// === pop() method ===
+// =================== pop() method ===================
+console.log('========= pop() ============')
 
 let fruit = ['apple', 'banana', 'orange'];
 console.log(fruit)
@@ -16,28 +16,51 @@ let lastFruit = fruit.pop();
 console.log(lastFruit)
 console.log(fruit)
 
+
+// =================== unshift() method ===================
 console.log('========= unshift() =========')
 
-// === unshift() method ===
+let fruit_unsifht = ['banana', 'orange'];
+console.log(fruit_unsifht)
+fruit_unsifht.unshift('apple', 'grape');
+console.log(fruit_unsifht)
 
-let frui = ['banana', 'orange'];
-console.log(frui)
-frui.unshift('apple', 'grape');
-console.log(frui)
 
+// =================== shift() ===================
 console.log('============ shift() =========')
 
-// === shift() ===
-
-let fru = ['apple', 'banana', 'orange'];
-console.log(fru)
-let firstFruit = fru.shift();
+let fruit_shift = ['apple', 'banana', 'orange'];
+console.log(fruit_shift)
+let firstFruit = fruit_shift.shift();
 console.log(firstFruit)
-console.log(fru)
+console.log(fruit_shift)
 
+
+// ===================== length ====================
+console.log('============ length property=========')
+
+let fruit_length = ['apple', 'banana', 'mango', 'orange']
+console.log(fruit_length.length)
+
+fruit_length.length = 2   // Truncating the array
+console.log(fruit_length)
+
+fruit_length.length = 6  // Expanding the array
+console.log(fruit_length)
+
+
+// =================== indexOf() method ===================
+console.log('========= indexOf() =========')
+
+let fruit_indexof = ['apple', 'banana', 'mango','orange','stawberry', 'banana',9]
+console.log('index of banana: ',fruit_indexof.indexOf('banana'))
+console.log('index of banana after 2 index:',fruit_indexof.indexOf('banana',2))
+console.log('index of digit 9: ', fruit_indexof.indexOf(9))
+
+
+// =================== concat() method ===================
 console.log('========= concat() =========')
 
-// === concat() method ===
 let fruits1 = ['apple', 'banana'];
 console.log(fruits1)
 let fruits2 = ['orange', 'grape'];
@@ -45,28 +68,28 @@ console.log(fruits2)
 let allFruits = fruits1.concat(fruits2);
 console.log(allFruits)
 
+
+// =================== slice() method ===================
 console.log('========= slice() =========')
 
-// === slice() method ===
-
-let fr = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
-let slicedFruits = fr.slice(1, 4);
-let slicedFruits1 = fr.slice(2); //start from index 2
+let fruit_slice = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+let slicedFruits = fruit_slice.slice(1, 4);
+let slicedFruits1 = fruit_slice.slice(2); //start from index 2
 console.log(slicedFruits)
 console.log(slicedFruits1)
 
+
+// =================== splice() method ===================
 console.log('========= splice() =========')
 
-// === splice() method ===
-
-let f = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
-f.splice(2, 1, 'mango', 'peach')
-console.log(f)
+let fruit_splice = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+fruit_splice.splice(2, 1, 'mango', 'peach')
+console.log(fruit_splice)
 
 
+
+// =================== for loop ==================
 console.log('========= for loop ========')
-
-// === for loop ==
 
 let fruitss = ['apple', 'banana', 'orange', 'grape'];
 
